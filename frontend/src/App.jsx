f843f87366a9d412e8c52ec9be1e502c72d6e144
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-07-16 08:35:43 +0700

    feat: implement routing with MainLayout and BookPage components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import BookPage from './page/BookPage';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<MainLayout />}>
+          <Route path='books' element={<BookPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
