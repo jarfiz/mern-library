05d3a8764a532fe5b9c396e995c7934957cbddef
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-07-16 09:41:27 +0700

    feat: add Navbar component and integrate it into MainLayout

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header className='shadow-sm'>
+      <nav className='mx-auto flex h-16 max-w-7xl items-center justify-between px-4'>
+        <div>
+          <Link to='/' className='text-2xl font-bold'>
+            Library
+          </Link>
+        </div>
+
+        <ul>
+          <li>
+            <Link to='/book'>Book</Link>
+          </li>
+        </ul>
+
+        <div className='flex items-center space-x-4'>
+          <div>
+            <button>Login</button>
+          </div>
+          <div>
+            <button>Register</button>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- frontend/src/layout/MainLayout.jsx
+++ frontend/src/layout/MainLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
 
 const MainLayout = () => {
   return (
     <div>
       {/* header */}
-
+      <Navbar />
       <main>
         <Outlet />
       </main>
