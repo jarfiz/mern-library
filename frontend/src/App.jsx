04dd48dd717d18fb5094e5167219018eae63de4a
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-07-16 09:34:28 +0700

    feat: add RegisterPage component and integrate it into routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import BookPage from './page/BookPage';
 import BookDetailPage from './page/BookDetailPage';
+import RegisterPage from './page/RegisterPage';
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<MainLayout />}>
+          <Route path='register' element={<RegisterPage />} />
           <Route path='books' element={<BookPage />} />
           <Route path='books/:id' element={<BookDetailPage />} />
         </Route>
